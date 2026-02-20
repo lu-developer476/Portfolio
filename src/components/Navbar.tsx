68885b57fa5b2cc42df195a70876be4f54fd7538
Lu <117615336+lu-developer476@users.noreply.github.com>
2026-02-20 16:03:45 -0300

    Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -62,7 +62,7 @@ export default function Navbar() {
               <div className="text-sm md:text-base font-semibold whitespace-nowrap">
                 Lucas Montenegro
               </div>
-              <div className="text-sm md:text-base text-cyber-neonGreen/60 font-mono tracking-wider">
+              <div className="text-sm text-cyber-neonGreen/60 font-mono tracking-wider">
                 Freelancer Frontend Developer · UX Interfaces
               </div>
             </Link>
