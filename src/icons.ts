47888592a968a5448859ad91dcc2f80ccf76b543
Lu <117615336+lu-developer476@users.noreply.github.com>
2026-02-24 04:30:35 -0300

    Create icons.ts

---
+++ src/icons.ts
@@ -0,0 +1,18 @@
+export { default as CSS3Icon } from "@/assets/svg/css3.svg";
+export { default as CoffeeScriptIcon } from "@/assets/svg/coffeescript.svg";
+export { default as HTML5Icon } from "@/assets/svg/html5.svg";
+export { default as JavaScriptIcon } from "@/assets/svg/javascript.svg";
+export { default as PythonIcon } from "@/assets/svg/python.svg";
+export { default as TypeScriptIcon } from "@/assets/svg/typescript.svg";
+
+export { default as BootstrapIcon } from "@/assets/svg/bootstrap.svg";
+export { default as DjangoIcon } from "@/assets/svg/django.svg";
+export { default as NextJSIcon } from "@/assets/svg/nextjs.svg";
+export { default as ReactIcon } from "@/assets/svg/react.svg";
+export { default as TailwindIcon } from "@/assets/svg/tailwindcss.svg";
+
+export { default as FirebaseIcon } from "@/assets/svg/firebasehosting.svg";
+export { default as GitHubIcon } from "@/assets/svg/github.svg";
+export { default as N8nIcon } from "@/assets/svg/n8n.svg";
+export { default as OpenAIIcon } from "@/assets/svg/openai.svg";
+export { default as GeminiIcon } from "@/assets/svg/gemini.svg";
