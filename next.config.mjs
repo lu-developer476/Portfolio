dda6d87b7d07b334ae661ce1383f00368049e173
Lu <117615336+lu-developer476@users.noreply.github.com>
2026-02-24 05:27:02 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,8 @@
+import withSvgr from "next-svgr";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  webpack(config) {
-    // Encontrar la regla existente para SVG
-    const fileLoaderRule = config.module.rules.find((rule) =>
-      rule.test?.test?.(".svg")
-    );
-
-    // Excluir svg de la regla original
-    if (fileLoaderRule) {
-      fileLoaderRule.exclude = /\.svg$/i;
-    }
-
-    // Agregar SVGR
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ["@svgr/webpack"],
-    });
-
-    return config;
-  },
 };
 
-export default nextConfig;
+export default withSvgr(nextConfig);
