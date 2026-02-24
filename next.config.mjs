7addcdd9e3fd820ee30c0f34385071077dff00a4
Lu <117615336+lu-developer476@users.noreply.github.com>
2026-02-24 05:17:12 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,20 @@
 const nextConfig = {
   reactStrictMode: true,
   webpack(config) {
+    const fileLoaderRule = config.module.rules.find(
+      (rule) => rule.test?.test?.('.svg')
+    );
+
+    if (fileLoaderRule) {
+      fileLoaderRule.exclude = /\.svg$/i;
+    }
+
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
-      use: ["@svgr/webpack"],
+      use: ['@svgr/webpack'],
     });
+
     return config;
   },
 };
