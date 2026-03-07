e393b01c078192a21a5476e0031f54ac33a663ed
Lu <117615336+lu-developer476@users.noreply.github.com>
2026-03-07 05:31:08 -0300

    Update icons.ts

--- src/icons.ts
+++ src/icons.ts
@@ -1,33 +1,30 @@
-export { default as CSS3Icon } from "@/assets/svg/css3.svg";
-export { default as CoffeeScriptIcon } from "@/assets/svg/coffeescript.svg";
-export { default as HTML5Icon } from "@/assets/svg/html5.svg";
-export { default as JavaScriptIcon } from "@/assets/svg/javascript.svg";
-export { default as PythonIcon } from "@/assets/svg/python.svg";
-export { default as TypeScriptIcon } from "@/assets/svg/typescript.svg";
-
-export { default as BootstrapIcon } from "@/assets/svg/bootstrap.svg";
-export { default as DjangoIcon } from "@/assets/svg/django.svg";
-export { default as NextJSIcon } from "@/assets/svg/nextjs.svg";
-export { default as ReactIcon } from "@/assets/svg/react.svg";
-export { default as TailwindIcon } from "@/assets/svg/tailwindcss.svg";
-
-export { default as FirebaseIcon } from "@/assets/svg/firebasehosting.svg";
-export { default as GitHubIcon } from "@/assets/svg/github.svg";
-export { default as N8nIcon } from "@/assets/svg/n8n.svg";
-export { default as OpenAIIcon } from "@/assets/svg/openai.svg";
-export { default as GeminiIcon } from "@/assets/svg/gemini.svg";
-
 export { default as AlpineJSIcon } from "@/assets/svg/alpinejs.svg";
 export { default as AnimeJSIcon } from "@/assets/svg/animejs.svg";
+export { default as BootstrapIcon } from "@/assets/svg/bootstrap.svg";
 export { default as ChartJSIcon } from "@/assets/svg/chartjs.svg";
+export { default as CoffeeScriptIcon } from "@/assets/svg/coffeescript.svg";
 export { default as CropperJSIcon } from "@/assets/svg/cropperjs.svg";
+export { default as CSS3Icon } from "@/assets/svg/css3.svg";
+export { default as DjangoIcon } from "@/assets/svg/django.svg";
+export { default as FirebaseIcon } from "@/assets/svg/firebasehosting.svg";
+export { default as GeminiIcon } from "@/assets/svg/gemini.svg";
+export { default as GitHubIcon } from "@/assets/svg/github.svg";
 export { default as HowlerJSIcon } from "@/assets/svg/howlerjs.svg";
+export { default as HTML5Icon } from "@/assets/svg/html5.svg";
+export { default as JavaScriptIcon } from "@/assets/svg/javascript.svg";
+export { default as N8nIcon } from "@/assets/svg/n8n.svg";
 export { default as NetlifyIcon } from "@/assets/svg/netlifyhosting.svg";
+export { default as NextJSIcon } from "@/assets/svg/nextjs.svg";
 export { default as NodeJSIcon } from "@/assets/svg/nodejs.svg";
+export { default as OpenAIIcon } from "@/assets/svg/openai.svg";
+export { default as PythonIcon } from "@/assets/svg/python.svg";
+export { default as ReactIcon } from "@/assets/svg/react.svg";
 export { default as RenderIcon } from "@/assets/svg/renderhosting.svg";
-
+export { default as SupabaseIcon } from "@/assets/svg/supabasehosting.svg";
 export { default as SweetAlert2Icon } from "@/assets/svg/sweetalert2.svg";
 export { default as SwiperJSIcon } from "@/assets/svg/swiperjs.svg";
+export { default as TailwindIcon } from "@/assets/svg/tailwindcss.svg";
 export { default as ThreeJSIcon } from "@/assets/svg/threejs.svg";
 export { default as ToneJSIcon } from "@/assets/svg/tonejs.svg";
+export { default as TypeScriptIcon } from "@/assets/svg/typescript.svg";
 export { default as VercelIcon } from "@/assets/svg/vercelhosting.svg";
