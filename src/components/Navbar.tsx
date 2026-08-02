9b653a3230fe2529c0e4ed5554ed51300b7e0fda
Lu <117615336+lu-developer476@users.noreply.github.com>
2026-08-02 20:49:05 -0300

    Fix clipped developer title (#90)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -116,7 +116,7 @@ export default function Navbar() {
               <div className="text-sm md:text-base font-semibold whitespace-nowrap">
                 Lucas Montenegro
               </div>
-              <div className="freelancer-role text-xs font-mono tracking-wider text-cyber-neonGreen/60 overflow-hidden border-r-2 border-cyber-neonGreen/80 whitespace-nowrap animate-typing w-[20ch]">
+              <div className="freelancer-role text-xs font-mono tracking-wider text-cyber-neonGreen/60 overflow-hidden border-r-2 border-cyber-neonGreen/80 whitespace-nowrap animate-typing w-[21ch]">
                 Full Stack Developer
               </div>
             </Link>
